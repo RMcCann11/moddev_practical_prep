7661c3d9e3f44c484d20072266e61009212fca0f
Ronan McCann <ronanemccann@hotmail.com>
2022-11-03 16:51:35 +0000

    Show title of planet & openDetails()

--- lab_prep/src/app/pages/planets/planets.page.ts
+++ lab_prep/src/app/pages/planets/planets.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-planets',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlanetsPage implements OnInit {
 
-  constructor() { }
+  planets : Observable<any>;
+
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
+    this.planets = this.http.get('https://swapi.dev/api/planets');
   }
+
+  openDetails(planet) {
+    let split = planet.url.split('/');
+    let planetId = split[split.length-2];
+    this.router.navigateByUrl(`/tabs/planets/${planetId}`);
+    }
 
 }
