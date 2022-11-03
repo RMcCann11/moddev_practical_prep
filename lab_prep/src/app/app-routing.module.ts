f02e0382f5fd6b7346a214cdb85a1deb9d0b16ec
Ronan McCann <ronanemccann@hotmail.com>
2022-11-03 16:24:13 +0000

    Generated planetDetails page

--- lab_prep/src/app/app-routing.module.ts
+++ lab_prep/src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)}
+  { path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)},
+  {
+    path: 'planet-details',
+    loadChildren: () => import('./pages/planet-details/planet-details.module').then( m => m.PlanetDetailsPageModule)
+  }
   //As per slide 12, we're setting the entry point to the TabsPage component
 ];
 
