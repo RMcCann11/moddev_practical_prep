215fba8369f226619af6a9a4cd019c88a04e0b5a
Ronan McCann <ronanemccann@hotmail.com>
2022-11-03 16:16:02 +0000

    WK 5-Implementation of ApiService for film(s)-Sl 39 & 40

--- lab_prep/src/app/pages/film-details/film-details.page.ts
+++ lab_prep/src/app/pages/film-details/film-details.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+//import { HttpClient } from '@angular/common/http';
+//Part of Handling of id passed by openDetails()-Sl 32 & 33
+import { ApiService } from 'src/app/services/api.service';
  
 
 @Component({
@@ -18,13 +20,20 @@ export class FilmDetailsPage implements OnInit {
   // constructor(private activatedRoute: ActivatedRoute) { }
   //Part of Capturing a value passed by URL - WK 4 Slide 25
 
-  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) { }
+  // constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) { }
+  //Part of Handling of id passed by openDetails()-Sl 32 & 33
+  constructor(private activatedRoute: ActivatedRoute, private api: ApiService) { }
 
   ngOnInit() {
     //this.filmId = this.activatedRoute.snapshot.paramMap.get('id');
     //Part of Capturing a value passed by URL - WK 4 Slide 25
+    // let id = this.activatedRoute.snapshot.paramMap.get('id');
+    // this.http.get(`https://swapi.dev/api/films/${id}`).subscribe((res) => {
+    //   this.film = res;
+    // });
+    //Part of Handling of id passed by openDetails()-Sl 32 & 33
     let id = this.activatedRoute.snapshot.paramMap.get('id');
-    this.http.get(`https://swapi.dev/api/films/${id}`).subscribe((res) => {
+    this.api.getFilm(id).subscribe((res) => {
       this.film = res;
     });
   }
--- lab_prep/src/app/pages/films/films.page.ts
+++ lab_prep/src/app/pages/films/films.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+//import { HttpClient } from '@angular/common/http';
+//NOTE THIS WAS CODE FROM SLIDES 28 & 29 Show title of film & openDetails()
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-films',
@@ -12,7 +14,9 @@ export class FilmsPage implements OnInit {
 
   films: Observable<any>;
 
-  constructor(private router: Router, private http: HttpClient) { }
+  // constructor(private router: Router, private http: HttpClient) { }
+  //NOTE THIS WAS CODE FROM SLIDES 28 & 29 Show title of film & openDetails()
+  constructor(private router: Router, private api: ApiService) { }
 
   ngOnInit() {
     // this.films = this.http.get('https://swapi.dev/api/films');
@@ -20,7 +24,9 @@ export class FilmsPage implements OnInit {
     //   console.log('my data: ', data);
     // });
     //NOTE THIS WAS CODE FROM SLIDE 25 TO DISPLAY DATA IN THE CONSOLE
-    this.films = this.http.get('https://swapi.dev/api/films');
+    //this.films = this.http.get('https://swapi.dev/api/films');
+    //NOTE THIS WAS CODE FROM SLIDES 28 & 29 Show title of film & openDetails()
+    this.films = this.api.getFilms();
   }
 
   openDetails(film) {
