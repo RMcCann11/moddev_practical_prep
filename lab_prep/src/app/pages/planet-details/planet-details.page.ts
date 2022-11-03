bb832e11a00a06fe9ea844ca79147278f9a65f31
Ronan McCann <ronanemccann@hotmail.com>
2022-11-03 17:05:04 +0000

    Handling of id passed by openDetails() to show planetDetails

--- lab_prep/src/app/pages/planet-details/planet-details.page.ts
+++ lab_prep/src/app/pages/planet-details/planet-details.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-planet-details',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlanetDetailsPage implements OnInit {
 
-  constructor() { }
+  planet: any;
+
+  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit() {
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.http.get(`https://swapi.dev/api/planets/${id}`).subscribe(res => {
+    this.planet = res;
+    });
   }
 
 }
