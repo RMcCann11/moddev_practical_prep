5fa251eddad36eb1266fdc37122c930225eb92ec
Ronan McCann <ronanemccann@hotmail.com>
2022-11-04 08:49:31 +0000

    Handling of id passed by openDetails() to show personDetails

--- lab_prep/src/app/pages/person-details/person-details.page.ts
+++ lab_prep/src/app/pages/person-details/person-details.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+//NOTE THIS WAS CODE FROM Handling of id passed by openDetails() to show personDetails i.e before API implementation
 
 @Component({
   selector: 'app-person-details',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonDetailsPage implements OnInit {
 
-  constructor() { }
+  person: any;
+
+  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) { }
+  //NOTE THIS WAS CODE FROM Handling of id passed by openDetails() to show personDetails i.e before API implementation
 
   ngOnInit() {
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.http.get(`https://swapi.dev/api/people/${id}`).subscribe(res => {
+    this.person = res;
+    });
+    //NOTE THIS WAS CODE FROM Handling of id passed by openDetails() to show personDetails i.e before API implementation
   }
 
 }
